ddfb636570d71d4829c31780e32758439d1f32b0
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2024-06-26 00:04:22 +0530

    delete Income func added and APi Checked

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -38,3 +38,13 @@ exports.getIncomes = async (req, res) =>{
     }
 }
 
+exports.deleteIncome = async (req, res) =>{
+    const {id} = req.params;
+    IncomeSchema.findByIdAndDelete(id)
+        .then((income) =>{
+            res.status(200).json({message: 'Income Deleted'})
+        })
+        .catch((err) =>{
+            res.status(500).json({message: 'Server Error'})
+        })
+}
--- backend/routes/transictions.js
+++ backend/routes/transictions.js
@@ -1,10 +1,11 @@
-const { addIncome, getIncomes } = require('../controllers/income');
+const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 
 const router = require('express').Router();
 
 
 router.post('/add-income', addIncome)
     .get('/get-incomes', getIncomes)
+    .delete('/delete-income/:id', deleteIncome)
   
 
 module.exports = router
