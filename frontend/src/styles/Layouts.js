3bdf3d966c65bdd803ac16f2b03257cd3165c907
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2024-07-01 19:11:41 +0530

    Responsivnes for 480px setteled for layouts

--- frontend/src/styles/Layouts.js
+++ frontend/src/styles/Layouts.js
@@ -5,9 +5,19 @@ export const MainLayout = styled.div`
     height: 100%;
     display: flex;
     gap: 2rem;
+
+    @media (max-width: 480px) {
+        padding: 1rem;
+        flex-direction: column;
+        gap: 1rem;
+    }
 `;
 
 export const InnerLayout = styled.div`
     padding: 2rem 1.5rem;
     width: 100%;
+
+    @media (max-width: 480px) {
+        padding: 1rem;
+    }
 `;
