a0564644cff168d8184770b6a1ceb52c7094253f
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2024-07-02 09:17:33 +0530

    final update

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "https://expense-sage-api.vercel.app";
+const BASE_URL = "https://expense-sage-api.vercel.app/api/v1/";
 
 
 const GlobalContext = React.createContext()
