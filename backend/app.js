ea8b3c6ed13c47ccf155d6344af6b966806e4cf0
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2024-07-02 05:30:11 +0530

    Vercel connecton

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,11 @@ require('dotenv').config()
 const PORT = process.env.PORT
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:["https://final-project-ten-theta.vercel.app"],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
