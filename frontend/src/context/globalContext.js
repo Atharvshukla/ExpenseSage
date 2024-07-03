e5871dbf5b78c6af8c2f7910326d15ebc9516dbd
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2024-07-03 19:21:46 +0530

    context file changes

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = process.env.REACT_APP_BASE_URL ||"http://localhost:5000/api/v1/";
+const BASE_URL = "https://expensesage.onrender.com/";
 
 
 const GlobalContext = React.createContext()
