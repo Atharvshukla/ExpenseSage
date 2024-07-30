cb16ddbc3d0f934b960621b062881bb4db4c55af
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2024-07-30 20:45:19 +0530

    Advise Feature Added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,125 +1,94 @@
-// NavbarComponent.js
 import React, { useCallback, useEffect, useState } from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import "./style.css";
 import { useNavigate } from 'react-router-dom';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-const Header = () => {
-  
-const navigate = useNavigate();
 
-  const handleShowLogin = () =>{
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleShowLogin = () => {
     navigate("/login");
-  }
+  };
 
   const [user, setUser] = useState();
 
   useEffect(() => {
-    
-      if (localStorage.getItem("user")) {
-        const user = JSON.parse(localStorage.getItem("user"));
-        
-        setUser(user);
-        
-      }
-
-
-    
+    if (localStorage.getItem("user")) {
+      const user = JSON.parse(localStorage.getItem("user"));
+      setUser(user);
+    }
   }, []);
 
   const handleShowLogout = () => {
     localStorage.removeItem("user");
     navigate("/login");
-  }
+  };
+
+  const handleShowInvestmentAdvice = () => {
+    window.location.href = "https://expensesage-advise.streamlit.app";
+  };
 
   const particlesInit = useCallback(async (engine) => {
-    // console.log(engine);
     await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(async (container) => {
     // await console.log(container);
   }, []);
-  
+
   return (
     <>
-    <div style={{ position: 'relative', overflow: 'hidden' }}>
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: '#000',
+      <div style={{ position: 'relative', overflow: 'hidden' }}>
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: { value: '#000' },
             },
-          },
-          fpsLimit: 60,
-          particles: {
-            number: {
-              value: 200,
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-            },
-            color: {
-              value: '#ffcc00',
-            },
-            shape: {
-              type: 'circle',
-            },
-            opacity: {
-              value: 0.5,
-              random: true,
-            },
-            size: {
-              value: 3,
-              random: { enable: true, minimumValue: 1 },
-            },
-            links: {
-              enable: false,
-            },
-            move: {
-              enable: true,
-              speed: 2,
-            },
-            life: {
-              duration: {
-                sync: false,
-                value: 3,
-              },
-              count: 0,
-              delay: {
-                random: {
+            fpsLimit: 60,
+            particles: {
+              number: {
+                value: 200,
+                density: {
                   enable: true,
-                  minimumValue: 0.5,
+                  value_area: 800,
                 },
-                value: 1,
+              },
+              color: { value: '#ffcc00' },
+              shape: { type: 'circle' },
+              opacity: { value: 0.5, random: true },
+              size: { value: 3, random: { enable: true, minimumValue: 1 } },
+              links: { enable: false },
+              move: { enable: true, speed: 2 },
+              life: {
+                duration: { sync: false, value: 3 },
+                count: 0,
+                delay: {
+                  random: { enable: true, minimumValue: 0.5 },
+                  value: 1,
+                },
               },
             },
-          },
-          detectRetina: true,
-        }}
-        style={{
-          position: 'absolute',
-          zIndex: -1,
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        }}
-      />
-    <Navbar className="navbarCSS" collapseOnSelect expand="lg" style={{position: 'relative', zIndex: "2 !important"}}>
-      {/* <Navbar className="navbarCSS" collapseOnSelect expand="lg" bg="dark" variant="dark"> */}
-        <Navbar.Brand href="/" className="text-white navTitle">Expense Management System</Navbar.Brand>
-        <Navbar.Toggle
+            detectRetina: true,
+          }}
+          style={{
+            position: 'absolute',
+            zIndex: -1,
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        />
+        <Navbar className="navbarCSS" collapseOnSelect expand="lg" style={{position: 'relative', zIndex: "2 !important"}}>
+          <Navbar.Brand href="/" className="text-white navTitle">Expense Management System</Navbar.Brand>
+          <Navbar.Toggle
             aria-controls="basic-navbar-nav"
-            style={{
-              backgroundColor: "transparent",
-              borderColor: "transparent",
-            }}
+            style={{ backgroundColor: "transparent", borderColor: "transparent" }}
           >
             <span
               className="navbar-toggler-icon"
@@ -128,26 +97,25 @@ const navigate = useNavigate();
               }}
             ></span>
           </Navbar.Toggle>
-        <div>
-        <Navbar.Collapse id="responsive-navbar-nav" style={{color: "white"}}>
-          {user ? (
-            <>
-            <Nav>
-                <Button variant="primary" onClick={handleShowLogout} className="ml-2">Logout</Button>
-              </Nav>
-            </>
-          ) : (
-
-            <>
-              <Nav>
-                <Button variant="primary" onClick={handleShowLogin} className="ml-2">Login</Button>
-              </Nav>
-            </>
-          )}
-          
-        </Navbar.Collapse>
-      </div>
-      </Navbar>
+          <div>
+            <Navbar.Collapse id="responsive-navbar-nav" style={{color: "white"}}>
+              {user ? (
+                <>
+                  <Nav>
+                    <Button variant="primary" onClick={handleShowInvestmentAdvice} className="ml-2">Investment Advice</Button>
+                    <Button variant="primary" onClick={handleShowLogout} className="ml-2">Logout</Button>
+                  </Nav>
+                </>
+              ) : (
+                <>
+                  <Nav>
+                    <Button variant="primary" onClick={handleShowLogin} className="ml-2">Login</Button>
+                  </Nav>
+                </>
+              )}
+            </Navbar.Collapse>
+          </div>
+        </Navbar>
       </div>
     </>
   );
